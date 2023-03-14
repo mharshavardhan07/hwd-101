ca3ef2abbfc5b5b5b7929590bbbb60329fd1aeec
mharshavardhan07 <126754284+mharshavardhan07@users.noreply.github.com>
2023-03-14 21:28:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const form=document.querySelector('form');
-const tbody=document.querySelector('tbody');
-form.addEventListener('submit',(event) =>{
-    event.preventDefault();
-    const name=form.elements.name.value;
-    const email=form.elements.email.value;
-    const password=form.elements.password.value;
-    const dob=form.elements.dob.value;
-    const termsAccepted=form.elements.terms.checked;
-    const row=document.createElement('tr');
-    row.innerHTML=`
-    <td>${name}</td>
-    <td>${email}</td>
-    <td>${password}</td>
-    <td>${dob}</td>
-    <td>${termsAccepted}</td>
-    `;
-    tbody.appendChild(row);
-    localStorage.setItem('entries' , tbody.innerHTML);
-    form.reset();})
-    
-    if (localStorage.getItem('entries')) {
-      tbody.innerHTML = localStorage.getItem('entries');
-    }
+const registrationForm = document.getElementById("registrationForm");
+		const registrationTable = document.getElementById("registrationTable");
+
+		
+		const data = JSON.parse(localStorage.getItem("registrationData")) || [];
+
+		
+		function updateTable() {
+			let table = "<tr><th>Name</th><th>Email</th><th>Password</th><th>dob</th><th>Accepted terms?</th></tr>";
+			for (let i = 0; i < data.length; i++) {
+				table += `<tr><td>${data[i].name}</td><td>${data[i].email}</td><td>${data[i].password}</td><td>${data[i].dob}</td><td>${data[i].acceptTerms}</td></tr>`;
+			}
+			registrationTable.innerHTML = table;
+		}
+
+		updateTable(); 
+
+
+		registrationForm.addEventListener("submit", function(event) {
+			event.preventDefault(); 
+
+			
+			const formData = new FormData(registrationForm);
+			const name = formData.get("name");
+			const email = formData.get("email");
+			const password = formData.get("password");
+			const dob = formData.get("dob");
+			const acceptTerms = formData.get("acceptTerms") ? "true" : "false";
+
+			
+			data.push({ name, email, password, dob, acceptTerms });
+			localStorage.setItem("registrationData", JSON.stringify(data));
+
+			updateTable(); 
+			registrationForm.reset();
+		});
